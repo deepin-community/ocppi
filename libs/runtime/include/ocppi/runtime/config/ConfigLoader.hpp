66efc0db82de81be6f9c08a9109aa0cc44f59628
black-desk <me@black-desk.cn>
2023-10-17 19:04:19 +0800

    New upstream version 0.1.8
    Signed-off-by: black-desk <me@black-desk.cn>

--- libs/runtime/include/ocppi/runtime/config/ConfigLoader.hpp
+++ libs/runtime/include/ocppi/runtime/config/ConfigLoader.hpp
@@ -1,17 +1,29 @@
-#include <bitset>    // for bitset, bitset<>::reference
-#include <cstddef>   // for size_t
-#include <exception> // for exception
-#include <iosfwd>    // for istream
-#include <list>
-#include <optional> // for optional
+#pragma once
 
-#include "ocppi/runtime/config/types/Config.hpp"
-#include "tl/expected.hpp"
+#include <exception> // for exception_ptr
+#include <iosfwd>    // for istream
+
+#include "tl/expected.hpp" // for expected
+
+namespace ocppi
+{
+namespace runtime
+{
+namespace config
+{
+namespace types
+{
+struct Config;
+} // namespace types
+} // namespace config
+} // namespace runtime
+} // namespace ocppi
 
 namespace ocppi::runtime::config
 {
 
 struct ConfigLoader {
+        [[nodiscard]]
         auto load(std::istream &in) const noexcept
                 -> tl::expected<types::Config, std::exception_ptr>;
 };
